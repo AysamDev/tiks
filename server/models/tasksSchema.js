4d97f397ee78efe179499ec6204e5c51ec6d3444
rubas1 <rubsaleh1@gmail.com>
2021-01-19 15:51:36 +0200

    backend done

--- server/models/tasksSchema.js
+++ server/models/tasksSchema.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const tasksSchema = new Schema({
-    tasks: [{type: Schema.Types.ObjectId, ref: 'Task'}],
-    date: Date
+    date: Date,
+    tasks: [{type: Schema.Types.ObjectId, ref: 'TaskModel'}]
 })
 
 const Tasks = mongoose.model("TasksModel", tasksSchema)
